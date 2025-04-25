69c942056f6d173859e941e4b87530e86d17befa
kev-fbx <ktran041203@gmail.com>
2025-04-26 05:32:31 +1000

    Initialise page info

---
+++ src/app/projects/projectData.tsx
@@ -0,0 +1,32 @@
+export const data = [
+    {
+      title: "Blender Contributions",
+      description: "Contributed to the Blender community by developing tools that speed up workflows.",
+      image: "/projectImg/blender.png",
+      link: "https://github.com/kev-fbx?tab=repositories&q=&type=public&language=python&sort=stargazers",
+    },
+    {
+      title: "Game Jam Winner: Exogenesis",
+      description: "#1 Judge's Choice Award and #9 People's Choice Award for the CISSAxGMC Game Jam 2025.",
+      image: "/projectImg/exogenesis.png",
+      link: "https://itch.io/jam/cissa-x-umgmc/rate/3388202",
+    },
+    {
+      title: "LEIDO",
+      description: "Animator for a  YouTube channel that reels  people into the world of Project Zomboid through 3D animation.",
+      image: "/projectImg/LEIDO.png",
+      link: "https://www.youtube.com/@LEIDO_PROD",
+    },
+    {
+      title: "Gravebound Trailer",
+      description: "Animated trailer for a client's multiplayer RPG server, for the video game Project Zomboid.",
+      image: "/projectImg/gravebound.png",
+      link: "https://www.youtube.com/watch?v=33sD8IvfcvM&ab_channel=GraveboundRPG",
+    },
+    {
+      title: "This website!",
+      description: "My portfolio website is developed using Next.js, Typescript, Three.js and deployed using Vercel.",
+      image: "/projectImg/portfolio.png",
+      link: "https://github.com/kev-fbx/kev-fbx-portfolio",
+    }
+  ];
